67a33a8a6193b0ca8fd0b78cc043b2397edad74e
Noviyanto Rahmadi <noviantorahmadi@gmail.com>
2022-11-12 20:42:16 +0800

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,69 +1,58 @@
-@extends('layouts.base')
-@section('title', $title)
+<script>
+    var params = '{!! request()->has('_pjax') ? '&' : '?' !!}datatable=true';
+    var table = Nue.components.NueDatatables.init('.js-datatable', {
+        scrollY: 'calc(100vh - 200px)',
+        ajax : '{!! request()->fullUrl() !!}' + params, 
+        columns: [
+            { data: 'pilihan', name: 'pilihan', className: 'pe-0 bg-light text-center', orderable: false, searchable: false },
+            { data: 'name', name: 'name' },
+            { data: 'email', name: 'email' },
+            { data: 'roles', name: 'roles', orderable: false, searchable: false },
+            { data: 'uid', name: 'uid', className: 'text-center', orderable: false, searchable: false },
+            { data: 'last_login_at', name: 'last_login_at', orderable: false, searchable: false },
+            { data: 'action', name: 'action', className: 'text-center', orderable: false, searchable: false }, 
+        ],
+        @include('nue::partials.datatable.script')
+    });
 
-@section('js')
-    <script src="https://aws.btekno.id/templates/front-dashboard/2.1/vendor/datatables/media/js/jquery.dataTables.min.js"></script>
-    <script src="https://aws.btekno.id/templates/front-dashboard/2.1/vendor/datatables.net.extensions/select/select.min.js"></script>
-    <script data-exec-on-popstate>
-        var table = Nue.components.NueDatatables.init('.js-datatable', {
-            scrollY: 'calc(100vh - 199px)',
-            ajax : '{!! request()->fullUrl() !!}?datatable=true', 
-            columns: [
-                { data: 'pilihan', name: 'pilihan', className: 'pe-0 bg-light text-center', orderable: false, searchable: false },
-                { data: 'id', name: 'id', className: 'text-center' },
-                { data: 'action', name: 'action', className: 'text-center', orderable: false, searchable: false }, 
-                { data: 'uid', name: 'uid' },
-                { data: 'name', name: 'name' },
-                { data: 'email', name: 'email' },
-                { data: 'roles', name: 'roles', orderable: false, searchable: false },
-                { data: 'last_login_at', name: 'last_login_at', orderable: false, searchable: false },
-            ],
-            @include('nue::partials.datatable.script')
-        });
-    </script>
-@endsection
-
-@section('content')
-    
-    @include('nue::partials.breadcrumb', ['lists' => [
-        __('Extensions') => 'javascript:;', 
-        $title => 'active'
-    ]])
-
-    @include('nue::partials.toolbar', [
-        'create' => route("$prefix.create"), 
-        'datatable' => true
+    @include('nue::partials.datatable.delete', [
+        'url' => route("$prefix.index")
     ])
+</script>
 
-    {!! Form::open(['method' => 'DELETE', 'route' => ["$prefix.destroy", 'hapus-all'], 'id' => 'submit-all']) !!}
+@include('nue::partials.breadcrumb', ['lists' => [
+    __('Extension') => 'javascript:;', 
+    $title => 'active'
+]])
 
-    	<div class="card border-0 shadow-none rounded-0">
-            <div class="card-body p-0">
-                <div class="table-responsive">
-                    <table id="datatable" class="js-datatable table table-sm table-hover table-bordered table-nowrap">
-                        <thead class="thead-light">
-                            <tr>
-                                <th class="pe-0" width="1">
-                                    <div class="form-check mb-0">
-                                        <input id="datatable-checkbox-check" type="checkbox" class="form-check-input">
-                                        <label class="form-check-label" for="check-all"></label>
-                                    </div>
-                                </th>
-                                <th width="1">ID</th>
-                                <th width="1"></th>
-                                <th>{{ __('SSO ID') }}</th>
-                                <th>{{ __('Name') }}</th>
-                                <th>{{ __('Email') }}</th>
-                                <th>{{ __('Roles') }}</th>
-                                <th>{{ __('Last Login') }}</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-            </div>
-            @include('nue::partials.datatable.footer')
+@include('nue::partials.toolbar', [
+    'create' => route("$prefix.create"), 
+    'delete' => true, 
+    'datatable' => true
+])
+
+<div class="card border-0 shadow-none rounded-0">
+    <div class="card-body p-0">
+        <div class="table-responsive">
+            <table id="datatable" class="js-datatable table table-sm table-hover table-bordered table-nowrap">
+                <thead class="thead-light">
+                    <tr>
+                        <th class="pe-0" width="1">
+                            <div class="form-check mb-0">
+                                <input id="datatable-checkbox-check" type="checkbox" class="form-check-input">
+                                <label class="form-check-label" for="check-all"></label>
+                            </div>
+                        </th>
+                        <th>{{ __('Name') }}</th>
+                        <th>{{ __('Email') }}</th>
+                        <th>{{ __('Roles') }}</th>
+                        <th>{{ __('SSO') }}</th>
+                        <th>{{ __('Last Login') }}</th>
+                        <th width="1"></th>
+                    </tr>
+                </thead>
+            </table>
         </div>
-
-    {!! Form::close() !!}
-
-@endsection
+    </div>
+    @include('nue::partials.datatable.footer')
+</div>
