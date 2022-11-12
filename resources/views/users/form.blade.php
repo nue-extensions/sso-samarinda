93147f584efbba75e88bb8adafd56d7cb4556f3f
Noviyanto Rahmadi <noviantorahmadi@gmail.com>
2022-11-12 20:45:56 +0800

    Update form.blade.php

--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -1,5 +1,51 @@
-<div class="card-body bg-light pb-10" style="min-height: calc(100vh - 130px);">
+<link rel="stylesheet" href="https://cdn.enterwind.com/template/epanel/css/separate/vendor/select2.min.css">
+<style type="text/css">
+    .select2-dropdown {
+        border: 1px solid #e5e5e5;
+    }
+    .select2-container--default .select2-selection--single {
+        border: 1px solid #eff0f7;
+        -webkit-border-radius: 2.5px;
+        border-radius: 2.5px;
+        height: 35px;
+    }
+    .select2-container--default .select2-selection--single .select2-selection__rendered {
+        line-height: 35px;
+    }
+    .select2-container--default .select2-selection--single .select2-selection__arrow {
+        height: 35px;
+        right: 7px;
+        width: 25px;
+    }
+</style>
 
+<script src="https://cdn.enterwind.com/template/epanel/js/lib/select2/select2.full.min.js"></script>
+<script>
+    Nue.components.NueTomSelect.init('.js-select');
+    $("#ajax-sso").select2({
+        ajax: {
+            url: 'https://sso.samarindakota.go.id/api/sso/findUser',
+            dataType: 'json',
+            delay: 250,
+            type: "POST",
+            data: function (params) {
+                return {
+                    search: params.term
+                };
+            },
+            processResults: function (response) {
+                return {
+                    results: response
+                };
+            },
+            cache: true
+        },
+        placeholder: 'Pilih Pengguna SSO',
+        minimumInputLength: 2
+    });
+</script>
+
+<div class="card-body">
     <div class="row mb-5">
         <div class="col-md-2">
             <h5 class="mt-1">{{ __('User ID') }}</h5>
@@ -17,7 +63,10 @@
                     <label class="form-label text-cap text-muted mb-1" for="roles[]">
                         {{ __('Pilih User') }}
                     </label>
-                    <select id="ajax-sso" class="form-control" name="pengguna"></select>
+                    <select id="ajax-sso" class="form-control mb-1" name="pengguna"></select>
+                    <span class="small">
+                        <a href="javascript:;" onclick="history.go(0)">Refresh</a> bila tidak muncul.
+                    </span>
                     {!! $errors->first('pengguna', '<span class="text-muted"><small>:message</small></span>') !!}
                 </div>
             @endisset
@@ -78,23 +127,12 @@
             </div>
         </div>
     </div>
-    <div class="mb-10">&nbsp;</div>
 </div>
 
-<div class="position-fixed start-50 bottom-0 translate-middle-x w-100 zi-99 mb-3" style="max-width: 40rem;">
-    <div class="card card-sm bg-dark border-dark mx-2">
-        <div class="card-body">
-            <div class="row justify-content-center justify-content-sm-between">
-                <div class="col"></div>
-                <div class="col-auto">
-                    <div class="d-flex gap-3">
-                        <button type="reset" class="btn btn-ghost-light">{{ __('Reset') }} </button>
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Save') }} 
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="card-footer bg-light border-bottom d-flex p-0">
+    <button type="reset" class="btn btn-secondary rounded-0 me-0">{{ __('Reset') }}</button>
+    <button type="submit" class="btn btn-success rounded-0">
+        <i class="bi bi-save me-1"></i>
+        {{ isset($edit) ? __('Save changes') : __('Save') }}
+    </button>
 </div>
